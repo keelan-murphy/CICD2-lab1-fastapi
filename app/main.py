e10e3a093465682b7d18a82e5a7730186a91c4e4
Keelan Murphy <150018737+keelan-murphy@users.noreply.github.com>
2026-09-21 16:00:58 +0000

    Added user read endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
-app = FastAPI(titel="Lab 1 - FastAPI")
+from app.schemas import UserCreate
+
+app = FastAPI(title="Lab 1 - FastAPI User API")
+
+users: list[UserCreate] = []
 
 @app.get("/health")
 def health():
@@ -9,3 +13,32 @@ def health():
 @app.get("/hello") 
 def hello(): 
     return {"message": "Some Message"} 
+
+
+@app.post("/api/users", status_code=status.HTTP_201_CREATED) 
+def add_user(new_user: UserCreate): 
+    for existing_user in users: 
+        if existing_user.user_id == new_user.user_id: 
+            raise HTTPException( 
+                status_code=status.HTTP_409_CONFLICT, 
+                detail="A user with this user_id already exists", 
+            ) 
+ 
+    users.append(new_user) 
+    return new_user 
+
+@app.get("/api/users") 
+def get_users(): 
+    return users 
+ 
+ 
+@app.get("/api/users/{user_id}") 
+def get_user(user_id: int): 
+    for existing_user in users: 
+        if existing_user.user_id == user_id: 
+            return existing_user 
+ 
+    raise HTTPException( 
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="User not found", 
+    ) 
